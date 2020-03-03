10a91a5c10f3f88909a9e266e7337b724282777f
Cibelle Montor <you@example.com>
2020-03-03 15:34:46 -0700

    deploying

--- server.js
+++ server.js
@@ -12,14 +12,19 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// Add routes, both API and view
+app.use(routes);
+
 // Connect to the Mongo DB
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/booksDB",
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => console.log("Connected to the booksDB!")
 );
-// Add routes, both API and view
-app.use(routes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
