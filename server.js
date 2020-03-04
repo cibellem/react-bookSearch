a81a02096a385669468a7653d9dfee3e83f39a57
Cibelle Montor <you@example.com>
2020-03-04 16:08:00 -0700

    deploying to heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static assets (usually on heroku)
-app.use("/", express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "build")));
 
 // Add routes, both API and view
 // app.use(routes);
