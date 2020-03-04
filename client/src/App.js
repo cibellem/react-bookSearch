db829df12288c6f0ec782bf7f95815049e863b46
Cibelle Montor <you@example.com>
2020-03-03 21:00:03 -0700

    stash

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SearchBox from "./Components/SearchBox/index";
 import NavBar from "./Components/Nav/index";
 import "./index.css";
-import Favorites from "./Pages/index";
+import Favorites from "./Pages/favorites/index";
 
 import BookCard from "./Components/BookCard/index";
 
@@ -14,13 +14,13 @@ function App() {
       <>
         <NavBar />
         <Switch>
+          <Route exact path="/" component={SearchBox} />
           <Route exact path="/favorites" component={Favorites} />
         </Switch>
         <h6 className="bookQuote">
           You don’t have to burn books to destroy a culture. Just get people to
           stop reading them.” – Ray Bradbury
         </h6>
-        <SearchBox />
 
         {!search ? <BookCard /> : null}
       </>
