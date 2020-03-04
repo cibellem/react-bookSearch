8b12cc84bdacc6ec376296f03d2c2a5fae823b6b
Cibelle Montor <you@example.com>
2020-03-03 18:48:25 -0700

    deploying

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ const path = require("path");
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
-app.use(express.static(path.join(__dirname, "client", "build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 // Add routes, both API and view
 app.use(routes);
