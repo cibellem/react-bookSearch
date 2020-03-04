ff66d87eadf214f0ca79bfc9eed21c71714b0877
Cibelle Montor <you@example.com>
2020-03-03 20:12:14 -0700

    stash

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// // Add routes, both API and view
+app.use(routes);
+
 // Connect to the Mongo DB
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/booksDB",
@@ -25,8 +28,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-// // Add routes, both API and view
-app.use(routes);
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
