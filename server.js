13b489ca695b281b2cd60f47eaf95e459d35b0e6
Cibelle Montor <you@example.com>
2020-03-04 14:29:06 -0700

    deploying to heroku

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+app.get("*", (request, response) => {
+  response.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 // Add routes, both API and view
 app.use(routes);
 
