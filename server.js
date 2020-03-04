035ff6a92669f7662a5905bcc30cc2d9662e5bc8
Cibelle Montor <you@example.com>
2020-03-04 16:04:44 -0700

    deploying to heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const routes = require("./routes");
+// const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -9,10 +9,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static assets (usually on heroku)
-app.use(express.static(path.join(__dirname, "build")));
+app.use("/", express.static(path.join(__dirname, "build")));
 
 // Add routes, both API and view
-app.use(routes);
+// app.use(routes);
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/booksDB",
   { useNewUrlParser: true, useUnifiedTopology: true },
