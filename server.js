92bb98a97a9ed54be47706c6ee7898a557ee0894
Cibelle Montor <you@example.com>
2020-03-04 15:39:27 -0700

    deploying to heroku

--- server.js
+++ server.js
@@ -8,20 +8,20 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Add routes, both API and view
-app.use(routes);
 // Serve up static assets (usually on heroku)
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
+// Add routes, both API and view
+app.use(routes);
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/booksDB",
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => console.log("Connected to the booksDB!")
 );
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
