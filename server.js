16fb1a0bfffdd3f6221b4a4188733100372dd39e
Cibelle Montor <you@example.com>
2020-03-03 20:21:12 -0700

    changing the server file one more time

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require("mongoose");
 const routes = require("./routes");
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use("/", express.static("client/build"));
 }
 
 // // Add routes, both API and view
