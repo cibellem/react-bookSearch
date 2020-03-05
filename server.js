71f892d2e01b3f1eeeca588795700dcb4900aaf0
Cibelle Montor <you@example.com>
2020-03-05 13:19:42 -0700

    deploying attemp

--- server.js
+++ server.js
@@ -1,24 +1,40 @@
-const express = require("express");
+//Importing Modules
+
 const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+//Importing files
 const routes = require("./routes");
+
+//Define globar variables
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
-app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/booksDB",
+  {
+    useNewUrlParser: true
+  },
   console.log("Connected to DB")
 );
+
+// configuration
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/", routes);
+
+// Serve up static assets (usually on heroku)
+
+//checks if application it's in production (heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Start the API server
 app.listen(PORT, function() {
