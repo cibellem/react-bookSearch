58bde1a750f4bad792b7d9548b8ab2275b97ced2
Cibelle Montor <you@example.com>
2020-03-05 14:42:52 -0700

    deployment

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const path = require("path");
 var express = require("express");
 var router = express.Router();
 
-const apiRoutes = require("../routes/api");
+const apiRoutes = require("./api");
 
 // API Routes
 router.use("/api", apiRoutes);
