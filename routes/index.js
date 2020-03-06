d4b84e78f487b96f1bfffcd726453d52ab7638bc
Cibelle Montor <you@example.com>
2020-03-05 20:20:54 -0700

    stash

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const path = require("path");
 var express = require("express");
 var router = express.Router();
 
-const apiRoutes = require("../routes/api/books");
+const apiRoutes = require("./api/index");
 
 // API Routes
 router.use("/api", apiRoutes);
