f9a08b01b4834a4556cad0048cfc6e240829dd71
Cibelle Montor <you@example.com>
2020-03-06 11:35:49 -0700

    trying new build

--- server.js
+++ server.js
@@ -11,15 +11,6 @@ dotenv.config();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI ||
-    "mongodb://userc:root123@ds145178.mlab.com:45178/heroku_td3h5gnm",
-  {}
-);
-// Add routes, both API and view
-require("./routes/API")(app);
-
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -27,6 +18,11 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+
+// Add routes, both API and view
+require("./routes/API")(app);
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/booksDb", {});
 
 app.listen(PORT, () => {
   console.log(`======> App listenning on  ${PORT}!`);
