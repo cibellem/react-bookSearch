116af2acc39e1c8ab1e6a32206532eb645df0d28
Cibelle Montor <you@example.com>
2020-03-06 10:55:27 -0700

    2nd attempt

--- server.js
+++ server.js
@@ -11,27 +11,23 @@ dotenv.config();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
-require("./routes/API")(app);
-
 // Connect to the Mongo DB
 mongoose.connect(
   process.env.MONGODB_URI ||
     "mongodb://userc:root123@ds145178.mlab.com:45178/heroku_td3h5gnm",
   {}
 );
+// Add routes, both API and view
+require("./routes/API")(app);
 
-app.get("*", (req, res) => {
-  let url = path.join(__dirname, "../client/build", "index.html");
-  if (!url.startsWith("/app/"))
-    // since we're on local windows
-    url = url.substring(1);
-  res.sendFile(url);
-});
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
   console.log(`======> App listenning on  ${PORT}!`);
 });
