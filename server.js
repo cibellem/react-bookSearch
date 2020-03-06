9f82eaaeb1cb781124c4085a2d133a6a4da420b7
Cibelle Montor <you@example.com>
2020-03-06 10:34:14 -0700

    1st friday attempt to deploy on heroku

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const routes = require("./routes");
 
 dotenv.config();
 
@@ -17,13 +16,14 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-app.use(routes);
+require("./routes/API")(app);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/booksDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb://userc:root123@ds145178.mlab.com:45178/heroku_td3h5gnm",
+  {}
+);
 
 app.get("*", (req, res) => {
   let url = path.join(__dirname, "../client/build", "index.html");
