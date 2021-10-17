9fbddfd385961d0950e15ee4f0b28758f45ec762
Cibelle Montor <montorcibelle@gmail.com>
2021-10-17 14:45:21 -0700

    updates port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
