0c9f41af0abc334863c450ebda2496646b1452be
Cibelle Montor <montorcibelle@gmail.com>
2021-10-17 12:25:30 -0700

    changes base url to heroku

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const apiRoutes = require("./API");
 
 // API Routes
-router.use("/api", apiRoutes);
+router.use("https://react-book-shelfie.herokuapp.com/api", apiRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
