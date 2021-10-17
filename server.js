9b820dc9043f41acfaba6ecf39894fe181a607c5
Cibelle Montor <montorcibelle@gmail.com>
2021-10-17 12:45:24 -0700

    adding cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
+const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
@@ -9,6 +10,7 @@ const routes = require("./routes");
 dotenv.config();
 
 // Define middleware here
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
