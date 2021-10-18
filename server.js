5fdbf86a731c07868bc6a5f01b775fcc40cb2874
Cibelle Montor <montorcibelle@gmail.com>
2021-10-17 19:17:41 -0700

    attemp to make work

--- server.js
+++ server.js
@@ -17,13 +17,11 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-const uri = process.env.MONGODB_URI;
-
 app.use(routes);
 // Connect to the Mongo DB
 mongoose.set("debug", true);
 mongoose.connect(
-  uri,
+  "mongodb+srv://cibellem:root@cluster0.bnk4x.mongodb.net/booksdb?retryWrites=true&w=majority",
   //DEV
   // process.env.MONGO_DB_DEV,
   {
